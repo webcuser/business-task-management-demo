58d031c313bf18c1972d5705833f6c342a0226b8
webcuser <97110714+webcuser@users.noreply.github.com>
2026-06-18 16:50:59 +0200

    feat: Create user management interface

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import UserList from './components/UserManagement/UserList';
+import './App.css';
+
+const App = () => {
+  return (
+    <div className="App">
+      <UserList />
+    </div>
+  );
+};
+
+export default App;
